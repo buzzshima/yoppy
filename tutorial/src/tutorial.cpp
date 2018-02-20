4d344bdbdba9c9bc7d17a8d45bead734e036c91e
buzzshima <senngoku77@gmail.com>
2018-02-20 13:43:44 +0900

    odom

--- tutorial/src/tutorial.cpp
+++ tutorial/src/tutorial.cpp
@@ -1,41 +1,27 @@
-#include<ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <turtlesim/Pose.h>
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
 
-class TurtleSim{
-	public:
-		TurtleSim();
-		void poseCallback(const turtlesim::PoseConstPtr& pose);
-
-	private:
-		ros::Publisher vel_pub;
-		ros::Subscriber pose_sub;
-		ros::NodeHandle nh;
-};
-
-TurtleSim::TurtleSim(){
-	vel_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
-	pose_sub = nh.subscribe<turtlesim::Pose>("turtle1/pose", 1, &TurtleSim::poseCallback, this);
+void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  ROS_INFO("Seq: [%d]", msg->header.seq);
+  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+  ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
 }
 
-void TurtleSim::poseCallback(const turtlesim::PoseConstPtr& pose){
-	ROS_INFO("%lf\t%lf",pose->x,pose->y);
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "odom_listener");
 
-	geometry_msgs::Twist nya;
-	nya.linear.x = 1.0;
-	nya.angular.z = 0.2;
+  
+  ros::NodeHandle n;
 
-	vel_pub.publish(nya);
+  
+  ros::Subscriber sub = n.subscribe("/icart_mini/odom", 1000, chatterCallback);
 
-}
-int main(int argc, char **argv){
-	
-	ros::init(argc, argv, "move_turtlesim");
-	TurtleSim turtleSim;
-	
-	ROS_INFO("Hello World");
-	while(ros::ok()){
-		ros::spin();
-	}
-	return 0;
+ 
+  ros::spin();
+
+  return 0;
 }
